9bf56ad25c96be166ef42c7ef3786fda9a79772c
Vincenzo Russo <vincenzo.russo@platform.sh>
2026-07-31 11:47:05 +0200

    Remove redundant search query subtitle

--- gg/themes/gratia-gratis/functions.php
+++ gg/themes/gratia-gratis/functions.php
@@ -112,7 +112,7 @@ function gratia_gratis_render_dynamic_books_preview( $block_content, $block ) {
 add_filter( 'render_block_core/group', 'gratia_gratis_render_dynamic_books_preview', 10, 2 );
 
 /**
- * Render the split search heading used by the Search template.
+ * Render the search heading used by the Search template.
  *
  * @return string
  */
@@ -123,11 +123,7 @@ function gratia_gratis_search_heading() {
 		return '<h1 class="wp-block-heading has-display-font-size">' . esc_html__( 'Search', 'gratia-gratis' ) . '</h1>';
 	}
 
-	return sprintf(
-		'<h1 class="wp-block-heading has-display-font-size">%1$s</h1><p class="gg-search-term has-large-font-size">%2$s</p>',
-		esc_html__( 'Search results for:', 'gratia-gratis' ),
-		esc_html( $search_query )
-	);
+	return '<h1 class="wp-block-heading has-display-font-size">' . esc_html__( 'Search results for:', 'gratia-gratis' ) . '</h1>';
 }
 add_shortcode( 'gratia_search_heading', 'gratia_gratis_search_heading' );
 
